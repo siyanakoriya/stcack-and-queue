169629671fe9993809c488865ca75f1e4ca0cf71
siyanakoriya <siyanakoriya122@gmail.com>
2022-09-15 18:35:55 +0530

    Stack

---
+++ src/com/bridgelaz/StackRunner.java
@@ -0,0 +1,16 @@
+package com.bridgelaz;
+
+
+public class StackRunner {
+
+    public static void main(String[] args) {
+
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        stack.print();
+
+    }
+}
